a2d747b0e562297ddd16cc0ec6df093292b81803
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-10-17 13:47:31 -0300

    Added NotFound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,22 @@
+import { Link } from 'react-router-dom';
+import './notfound.css';
+
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="notfound-wrapper">
+      <h3>
+        No se encontró esta página, por favor intenta una de las siguientes
+      </h3>
+      <ul>
+        <Link to="/login">
+          <li>Log in</li>
+        </Link>
+        <Link to="/documents">
+          <li>Documents</li>
+        </Link>
+      </ul>
+    </div>
+  );
 }
 
 export default NotFound;
