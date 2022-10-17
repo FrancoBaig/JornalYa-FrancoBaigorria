4a9b0edfebbf3ccdb519e5dabeed3b0b6f447f12
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-10-17 13:14:43 -0300

    Added document detail header

--- src/pages/Documents/Details/DocumentDetail.jsx
+++ src/pages/Documents/Details/DocumentDetail.jsx
@@ -1,18 +1,35 @@
+import { useNavigate } from 'react-router-dom';
 import Modal from '../../../components/Modal/Modal';
 import PDFViewer from '../../../components/PDFViewer/PDFViewer';
+import './documentDetails.css';
 
 function Loader() {
   return <h3>loading..</h3>;
 }
 
 function DocumentDetail({ pdf, loading, error }) {
+  const navigate = useNavigate();
   if (loading || error || !pdf) {
     <Loader />;
   }
 
+  function handleClose() {
+    navigate('/documents');
+  }
+
   return (
     <Modal>
-      <PDFViewer document={pdf} />
+      <div className="details-wrapper">
+        <div className="modal-header">
+          <h2 className="modal-header__title">
+            Recibos de sueldo de: Matias Romero
+          </h2>
+          <button className="btn btn--primary" onClick={handleClose}>
+            Cerrar
+          </button>
+        </div>
+        <PDFViewer document={pdf} />
+      </div>
     </Modal>
   );
 }
